4de11b16d2e6fb0e0063bb3912f1416e9f5aaf44
mmelly24 <mmelly24@users.noreply.github.com>
2021-04-25 15:27:35 +0200

    Routing

--- applicationProducteurs/imports/ui/body/body.js
+++ applicationProducteurs/imports/ui/body/body.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 import { ObjetAnnonce } from '../../api/annonces.js';
 
 import './body.html';
+import '../imports/ui/body/body.js';
 
 Template.body.helpers({
     annonces() {
