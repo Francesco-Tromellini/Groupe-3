b47afc015218c0c81ea1aec116f5a5a636c2faf0
leonienussbaum <80388012+leonienussbaum@users.noreply.github.com>
2021-05-03 17:03:37 +0200

    menu déroulant qui fonctionne !

--- applicationProducteurs/imports/ui/sidebar/navigation/navigation.js
+++ applicationProducteurs/imports/ui/sidebar/navigation/navigation.js
@@ -1,22 +1,40 @@
 import './navigation.html';
 import './navigation.css';
 
+//event 1er sous menu
 Template.navigation.events({
-    'mouseover': function() {
-        if (this.id = 'art') {
-            const art = document.getElementById('art_sous');
-            art.style.display = 'block';
-        }
-        
+    'mouseover .art': function() {
+            const a = document.getElementsByClassName('art_sous');
+            for (let i = 0; i <= 1; i++) {
+                a[i].style.display = 'block';
+            }
     }
 });
 
 Template.navigation.events({
-    'mouseout': function() {
-        if (this.id = 'art') {
-            const art = document.getElementById('art_sous');
-            art.style.display = 'none';
-        }
-        
+    'mouseout .art': function() {
+            const a = document.getElementsByClassName('art_sous');
+            for (let i = 0; i <= 1; i++) {
+                a[i].style.display = 'none';
+            }
+    }
+});
+
+// event 2e sous menu
+Template.navigation.events({
+    'mouseover .denr': function() {
+            const d = document.getElementsByClassName('denr_sous');
+            for (let i = 0; i <= 1; i++) {
+                d[i].style.display = 'block';
+            }
+    }
+});
+
+Template.navigation.events({
+    'mouseout .denr': function() {
+            const d = document.getElementsByClassName('denr_sous');
+            for (let i = 0; i <= 1; i++) {
+                d[i].style.display = 'none';
+            }
     }
 });
