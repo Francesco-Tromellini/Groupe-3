0fd79874e1bae7fe88279acfdc25a3b60b3b2528
mmelly24 <mmelly24@users.noreply.github.com>
2021-05-10 22:20:09 +0200

    tentative téléchargement pp

--- applicationProducteurs/imports/api/annonces.js
+++ applicationProducteurs/imports/api/annonces.js
@@ -1,4 +1,32 @@
 import { Mongo } from 'meteor/mongo';
 
+//import { FilesCollection} from 'meteor/ostrio:files';
+
 // Variable DB
 export const ObjetAnnonce = new Mongo.Collection('ObjetAnnonce');
+
+
+/*
+//nouvelle FS collection en lien avec notre package
+ const ProfileImages = new FilesCollection({
+     collectionName: "ProfileImages",
+     allowClientCode: true,
+     onBeforeUpload(file) {
+        // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+        if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+          return true;
+        }
+        return 'Please upload image, with size equal or less than 10MB';
+      }
+    });
+    
+    if (Meteor.isClient) {
+      Meteor.subscribe('files.images.all');
+    }
+    
+    if (Meteor.isServer) {
+      Meteor.publish('files.images.all', function () {
+        return ProfileImages.find().cursor;
+      });
+    }
+    */
--- applicationProducteurs/imports/ui/sidebar/profil/profil.js
+++ applicationProducteurs/imports/ui/sidebar/profil/profil.js
@@ -1,2 +1,15 @@
 import './profil.html';
 import './profil.css';
+
+
+Template.profil.events({
+    //commit to the db
+    "submit .edit-profile": function(event) {
+        var file = $('#profileImage').get(0).files[0];
+
+        if (file) {
+            //faire un nouveau fs file avec le package que l'on a téléchargé
+            fsFile = new FS.File(file);
+        }
+    }
+})
