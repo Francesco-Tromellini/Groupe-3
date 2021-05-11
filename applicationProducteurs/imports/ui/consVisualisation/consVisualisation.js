79133ca5c173e34a8704eb5fd6eea26277d2266a
BechirB2 <80389619+BechirB2@users.noreply.github.com>
2021-05-11 10:10:05 +0200

    Encore des changements
    Changements au fichier consVisualisation

--- applicationProducteurs/imports/ui/consVisualisation/consVisualisation.js
+++ applicationProducteurs/imports/ui/consVisualisation/consVisualisation.js
@@ -1,3 +1,18 @@
 import './consVisualisation.html';
 import './cons_sidebar/cons_side.js';
 import '../visualisation/visualisation.js'
+
+import '../annonce/annonce.js';
+import '../sidebar/sidebar.js';
+
+// Mots clef
+import { Template } from 'meteor/templating';
+
+// Importer DB
+import { ObjetAnnonce } from '../../api/annonces.js';
+
+Template.visualisation.helpers({
+    objet () {
+        return ObjetAnnonce.find({}, { sort: { createdAt: 1}});
+    },
+});
