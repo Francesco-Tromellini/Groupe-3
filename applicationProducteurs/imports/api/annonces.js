a2ed0b305ddd688c0ac04474ec88b800b23599cf
mmelly24 <mmelly24@users.noreply.github.com>
2021-05-12 10:03:54 +0200

    Télécharger images

--- applicationProducteurs/imports/api/annonces.js
+++ applicationProducteurs/imports/api/annonces.js
@@ -1,9 +1,9 @@
 import { Mongo } from 'meteor/mongo';
 
-//import { FilesCollection} from 'meteor/ostrio:files';
 
 // Variable DB
 export const ObjetAnnonce = new Mongo.Collection('ObjetAnnonce');
+
 
 
 /*
