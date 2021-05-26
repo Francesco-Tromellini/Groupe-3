e8b10f3e094720a058b5d7e4cd97c41f4ef08cbb
mmelly24 <mmelly24@users.noreply.github.com>
2021-05-26 11:15:35 +0200

    photo de profil consommateur

--- applicationProducteurs/imports/ui/consVisualisation/cons_sidebar/cons_profil/cons_profil.js
+++ applicationProducteurs/imports/ui/consVisualisation/cons_sidebar/cons_profil/cons_profil.js
@@ -1,10 +1,5 @@
 import './cons_profil.html';
 import '../../../sidebar/profil/images.js';
-import Images from '../../../../api/images.js';
 
-//afficher l'image
-Template.profil.helpers({
-  imageFile() {
-    return Images.findOne();
-  },
-});
+
+
