c34dfe8e6be7d548359abeda0f7d8f1d5b5c9b5e
mmelly24 <mmelly24@users.noreply.github.com>
2021-05-26 10:25:46 +0200

    commentaire profil et sidebar

--- applicationProducteurs/imports/ui/sidebar/profil/profil.js
+++ applicationProducteurs/imports/ui/sidebar/profil/profil.js
@@ -3,13 +3,4 @@ import './profil.css';
 import './images.js';
 import '../sidebar.js'
 
-import Images from '../../../api/images.js';
 
-//afficher l'image
-Template.profil.helpers({
-  imageFile() {
-    console.log(Images.findOne())
-    return Images.findOne();
-    
-  },
-});
--- applicationProducteurs/imports/ui/sidebar/sidebar.js
+++ applicationProducteurs/imports/ui/sidebar/sidebar.js
@@ -5,13 +5,15 @@ import './navigation/navigation.js';
 import './likes/likes.js';
 import './profil/images.js';
 
-
+// Mots clef
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { Meteor } from 'meteor/meteor'
 
+//importer BD
 import Images from '../../api/images.js';
 
+//retourner l'image de profil de l'utilisateur
 Template.sidebar.helpers({
     imageFile(){
         return Images.findOne();
@@ -20,15 +22,14 @@ Template.sidebar.helpers({
 
 Template.sidebar.events({
     'change .profilPicture': function () {
+        //récupérer le contenu qui a été stocké dans images.js
         let idProfil = Session.get('idProfil');
 
         Session.set('idProfil', '');
 
+        //changer la valeur de la photo de profil dans la BD de l'utilisateur
         Meteor.users.update({_id: Meteor.userId() }, { $set: { 'profile.idProfil': idProfil } });
 
-        console.log(Meteor.user().profile.idProfil);
-
-    
 
     }
 })
