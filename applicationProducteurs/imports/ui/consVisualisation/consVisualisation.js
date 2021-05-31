05ac5318c86f15ea53756e5a09fdc8606c15bd65
leonienussbaum <80388012+leonienussbaum@users.noreply.github.com>
2021-05-31 09:49:24 +0200

    rercherche par code postal et allègement du code

--- applicationProducteurs/imports/ui/consVisualisation/consVisualisation.js
+++ applicationProducteurs/imports/ui/consVisualisation/consVisualisation.js
@@ -9,134 +9,83 @@ import { Template } from 'meteor/templating';
 
 import { ObjetAnnonce } from '../../api/annonces.js';
 
-// helpers pour récup les annonces par région
-Template.consVisualisation.helpers ({
-  annonceregion() {
-    let input = document.getElementById('codepostal').value;
-    return ObjetAnnonce.find({ postalCode: input }, { sort: { createdAt: -1 } } );
-  }
-});
+import { ReactiveVar } from 'meteor/reactive-var';
+
+// déclarer la variable réactive
+Template.consVisualisation.onCreated(function(){
+  this.codePostal = new ReactiveVar( null );
+})
 
 // event pour rechercher par code postal
 Template.consVisualisation.events({
-  "click #rechercher": function filtreregion() {
-    const annonceparcode = document.getElementById('annonceparcode');
-    document.getElementById('corps').innerHTML = "";
-    annonceparcode.removeAttribute("hidden");
-    
+  "click #rechercher": function filtreregion(event, template) {
+    let input = document.getElementById('codepostal').value;
+    template.codePostal.set(ObjetAnnonce.find({ postalCode: input }, { sort: { createdAt: -1 } } ));
+    document.getElementById('corps').setAttribute("hidden", "");
+    document.getElementById('annonceparcode').removeAttribute("hidden");
   }
 });
 
-// affichage de toute les annonces
+
 Template.consVisualisation.helpers({
   objet() {
-    return ObjetAnnonce.find({}, { sort: { createdAt: -1 } });
+    return ObjetAnnonce.find({}, { sort: { createdAt: -1 } }); // affichage de toutes les annonces
   },
-  imageFile() {
-    return ImagesAnnonces.findOne();
+  annonceregion() {
+    return Template.instance().codePostal.get() // récupérer les annonces qui ont le même code postal que l'input
   },
+  bijoux() {
+    return ObjetAnnonce.find({ filtreSelected: "bijoux"}, { sort: { createdAt: -1 } } );// affichage des annonces portant le filtre "bijoux"
+  },
+  vêtements() {
+    return ObjetAnnonce.find({ filtreSelected: "vêtements"}, { sort: { createdAt: -1 } } ); // affichage des annonces portant le filtre "vêtement"
+  },
+  maraicher() {
+    return ObjetAnnonce.find({ filtreSelected: "maraîcher"}, { sort: { createdAt: -1 } } ); // affichage des annonces portant le filtre "maraîcher"
+  },
+  poissonier() {
+    return ObjetAnnonce.find({ filtreSelected: "poissonnier"}, { sort: { createdAt: -1 } } ); // affichage des annonces portant le filtre "poissonier"
+  }
+
 });
 
-// event sur le bouton accueil
+// events sur la sidebar 
 Template.cons_sidebar.events({
   'click #accueil': function () {
     document.getElementById('corps').removeAttribute("hidden"); // afficher à nouveau toutes les annonces
-    const filtre = document.getElementsByClassName('filtre'); 
-    for (let j = 0; j<= filtre.length; j++) {
-      filtre[j].setAttribute("hidden", ""); // retirer tous les affichage par filtre
-    }
-  }
-});
-
-// affichage des annonces portant le filtre "bijoux"
-Template.consVisualisation.helpers({
-  bijoux() {
-    return ObjetAnnonce.find({ filtreSelected: "bijoux"}, { sort: { createdAt: -1 } } );
+    document.getElementById('filtrebijoux').setAttribute("hidden", "");
+    document.getElementById('filtrevêtements').setAttribute("hidden", "");
+    document.getElementById('filtremaraîcher').setAttribute("hidden", "");
+    document.getElementById('filtrepoissonier').setAttribute("hidden", "");
   },
-  imageFile() {
-    return ImagesAnnonces.findOne();
-  },
-});
-
-// évènement de clic qui n'affiche que les annonces avec le filtre bijoux (même logique pour le reste des events)
-Template.cons_sidebar.events({
   'click #bijoux': function () {
-    document.getElementById('corps').setAttribute("hidden", ""); // retirer l'affichage de toutes annonces
-    const filtre = document.getElementsByClassName('filtre'); // récuper tous les div de la classe "filtre" dans un tableau
-    for (let j = 0; j<= filtre.length; j++) { // itérer sur tous les éléments du tableau
-      filtre[j].setAttribute("hidden", ""); // cacher tous les éléments de classe filtre
-      if (j == 0) { // condition pour séléctionner le div correspondant à la catégorie "bijoux" 
-        filtre[j].removeAttribute("hidden"); // afficher cet élément 
-      }
-    }
-  }
-});
-// affichage des annonces portant le filtre "vêtement"
-Template.consVisualisation.helpers({
-  vêtements() {
-    return ObjetAnnonce.find({ filtreSelected: "vêtements"}, { sort: { createdAt: -1 } } );
+    document.getElementById('filtrebijoux').removeAttribute("hidden"); // afficher à nouveau toutes les annonces
+    document.getElementById('corps').setAttribute("hidden", "");
+    document.getElementById('filtrevêtements').setAttribute("hidden", "");
+    document.getElementById('filtremaraîcher').setAttribute("hidden", "");
+    document.getElementById('filtrepoissonier').setAttribute("hidden", "");
   },
-  imageFile() {
-    return ImagesAnnonces.findOne();
-  },
-});
-
-
-Template.cons_sidebar.events({
   'click #vetements': function () {
+    document.getElementById('filtrevêtements').removeAttribute("hidden"); // afficher à nouveau toutes les annonces
     document.getElementById('corps').setAttribute("hidden", "");
-    const filtre = document.getElementsByClassName('filtre');
-    for (let j = 0; j<= filtre.length; j++) {
-      filtre[j].setAttribute("hidden", "");
-      if (j == 1) {
-        filtre[j].removeAttribute("hidden");
-      }
-    }
-  }
-});
-
-// affichage des annonces portant le filtre "maraîcher"
-Template.consVisualisation.helpers({
-  maraicher() {
-    return ObjetAnnonce.find({ filtreSelected: "maraîcher"}, { sort: { createdAt: -1 } } );
+    document.getElementById('filtrebijoux').setAttribute("hidden", "");
+    document.getElementById('filtremaraîcher').setAttribute("hidden", "");
+    document.getElementById('filtrepoissonier').setAttribute("hidden", "");
   },
-  imageFile() {
-    return ImagesAnnonces.findOne();
-  },
-});
-
-Template.cons_sidebar.events({
   'click #maraicher': function () {
+    document.getElementById('filtremaraîcher').removeAttribute("hidden"); // afficher à nouveau toutes les annonces
     document.getElementById('corps').setAttribute("hidden", "");
-    const filtre = document.getElementsByClassName('filtre');
-    for (let j = 0; j<= filtre.length; j++) {
-      filtre[j].setAttribute("hidden", "");
-      if (j == 2) {
-        filtre[j].removeAttribute("hidden");
-      }
-    }
-  }
-});
-
-// affichage des annonces portant le filtre "poissonier"
-Template.consVisualisation.helpers({
-  poissonier() {
-    return ObjetAnnonce.find({ filtreSelected: "poissonnier"}, { sort: { createdAt: -1 } } );
+    document.getElementById('filtrebijoux').setAttribute("hidden", "");
+    document.getElementById('filtrevêtements').setAttribute("hidden", "");
+    document.getElementById('filtrepoissonier').setAttribute("hidden", "");
   },
-  imageFile() {
-    return ImagesAnnonces.findOne();
-  },
-});
-
-Template.cons_sidebar.events({
   'click #poissonier': function () {
+    document.getElementById('filtrepoissonier').removeAttribute("hidden"); // afficher à nouveau toutes les annonces
     document.getElementById('corps').setAttribute("hidden", "");
-    const filtre = document.getElementsByClassName('filtre');
-    for (let j = 0; j<= filtre.length; j++) {
-      filtre[j].setAttribute("hidden", "");
-      if (j == 3) {
-        filtre[j].removeAttribute("hidden");
-      }
-    }
-  }
+    document.getElementById('filtrebijoux').setAttribute("hidden", "");
+    document.getElementById('filtrevêtements').setAttribute("hidden", "");
+    document.getElementById('filtremaraîcher').setAttribute("hidden", "");
+  },
+  
+
 });
